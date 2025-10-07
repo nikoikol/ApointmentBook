020178ea03aa698164faef379cbe22637dfb2d81
NikolayShevchenko <nikolays2@ncystudents.net>
2025-10-07 15:15:09 -0400

    Initial commit

---
+++ src/AppointmentBook.java
@@ -0,0 +1,36 @@
+public class AppointmentBook
+{
+    private boolean [][] schedule;
+
+    public AppointmentBook(boolean[][] schedule)
+    {
+        this.schedule = schedule;
+    }
+    private boolean isMinuteFree(int period, int minute)
+    {
+        return schedule[period -1][minute];
+    }
+    public int findFreeBlock(int period, int duration)
+    {
+        int block = 0;
+        for(int i = 0; i < 60; i++)
+        {
+            if(isMinuteFree(period, i))
+            {
+                block++;
+                if(block == duration)
+            }
+        }
+    }
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration)
+    {
+        return false;
+    }
+    public void printPeriod(int period)
+    {
+        for(int i = 0; i < schedule[period-1].length; i++)
+        {
+            System.out.println(i + " " + schedule[period-1][i]);
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+    boolean[][] schedule = new boolean [8][60];
+    for(int i = 10; i < 15; i++)
+    {
+        schedule[1][i] = true;
+    }
+    for(int i = 30; i < 44; i++)
+        {
+            schedule[1][i] = true;
+        }
+    for(int i = 50; i < 59; i++)
+        {
+            schedule[1][i] = true;
+        }
+    AppointmentBook a = new AppointmentBook(schedule);
+    a.printPeriod(2);
+    }
+}
